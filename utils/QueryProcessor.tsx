fc333051be24d58aea99a5f787e53f692b3836ae
Raul Melendez <raulmelendez088@gmail.com>
2026-02-23 11:33:17 -0500

    Changed name query to say my name. Added andrew id query and confirmed it returns my name

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -8,7 +8,11 @@ export default function QueryProcessor(query: string): string {
   }
 
   if (query.toLowerCase().includes("name")) {
-    return "Rohan";
+    return "Raul";
+  }
+
+  if (query.toLowerCase().includes("andrew id")) {
+    return "janayame";
   }
 
   return "";
