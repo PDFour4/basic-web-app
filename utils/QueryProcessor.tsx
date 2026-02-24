08b7c75580a23565db5d2cc485e31c8dd1fc2363
Raul Melendez <raulmelendez088@gmail.com>
2026-02-24 15:40:08 +0000

    Implemented scrabble functionality

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -177,5 +177,23 @@ export default function QueryProcessor(query: string): string {
     return evaluateMixedExpression(query);
   }
 
+  // Fifth Round queries
+  if(query.toLowerCase().includes("Scrabble")) {
+    const letterValues: { [key: string]: number } = {
+      A: 1, E: 1, I: 1, O: 1, U: 1, L: 1, N: 1, S: 1, T: 1, R: 1,
+      D: 2, G: 2,
+      B: 3, C: 3, M: 3, P: 3,
+      F: 4, H: 4, V: 4, W: 4, Y: 4,
+      K: 5,
+      J: 8, X: 8,
+      Q: 10, Z: 10
+    };
+    
+    const letters = query.match(/[a-zA-Z]/g);
+    if (letters) {
+      const score = letters.reduce((acc, letter) => acc + (letterValues[letter.toUpperCase()] || 0), 0);
+      return score.toString();
+    }
+  }
   return "";
 }
